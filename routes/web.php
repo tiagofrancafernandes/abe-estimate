5074bff8cf1c54368f44ab0e7d1c3b4b2bd3965d
Tiago França <tiago@tiagofranca.com>
2022-06-11 00:20:54 -0300

    users route group - prefix 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,20 @@ Route::get('/', function () {
     return redirect()->route('users.index');
 })->name('index');
 
-Route::get('/users/', [UserControler::class, 'index'])->name('users.index');
-Route::get('/users/create', [UserControler::class, 'create'])->name('users.create');
-Route::post('/users/store', [UserControler::class, 'store'])->name('users.store');
-Route::get('/users/{userId}', [UserControler::class, 'show'])->name('users.show');
-Route::get('/users/{userId}/edit', [UserControler::class, 'edit'])->name('users.edit');
-Route::post('/users/{userId}/update', [UserControler::class, 'update'])->name('users.update');
-Route::get('/users/{userId}/confirm_delete', [UserControler::class, 'confirmDelete'])->name('users.confirm_delete');
-/*
-Route::match([
-    'get',
-    'post',
-    'delete',
-], '/users/{userId}/delete', [UserControler::class, 'delete'])->name('users.delete');
-*/
-Route::delete('/users/{userId}/delete', [UserControler::class, 'delete'])->name('users.delete');
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserControler::class, 'index'])->name('users.index');
+    Route::get('/create', [UserControler::class, 'create'])->name('users.create');
+    Route::post('/store', [UserControler::class, 'store'])->name('users.store');
+    Route::get('/{userId}', [UserControler::class, 'show'])->name('users.show');
+    Route::get('/{userId}/edit', [UserControler::class, 'edit'])->name('users.edit');
+    Route::post('/{userId}/update', [UserControler::class, 'update'])->name('users.update');
+    Route::get('/{userId}/confirm_delete', [UserControler::class, 'confirmDelete'])->name('users.confirm_delete');
+    Route::delete('/{userId}/delete', [UserControler::class, 'delete'])->name('users.delete');
+    /*
+    Route::match([
+        'get',
+        'post',
+        'delete',
+    ], '/users/{userId}/delete', [UserControler::class, 'delete'])->name('users.delete');
+    */
+});
