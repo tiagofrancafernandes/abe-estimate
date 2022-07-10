1f917ba56dbd1030c0ff7fa64a589cc367c81159
Tiago França <dev@tiagofranca.com>
2022-07-10 12:14:37 -0300

    updated auth form
    No ramo feat/auth
    Mudanças a serem submetidas:
    	modified:   public/css/admin/auth.css
    	modified:   resources/views/auth/login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,9 +16,9 @@
                 <div id="login">
                     <h1>Welcome Back!</h1>
                     <form action="/" method="post">
-                        <div class="field-wrap"> <label> Email Address<span class="req">*</span> </label> <input
+                        <div class="field-wrap"> <label> Email Address<span class="req">*</span> </label> <input increment-id="1"
                                    type="email"required autocomplete="off" /> </div>
-                        <div class="field-wrap"> <label> Password<span class="req">*</span> </label> <input
+                        <div class="field-wrap"> <label> Password<span class="req">*</span> </label> <input increment-id="2"
                                    type="password"required autocomplete="off" /> </div>
                         <p class="forgot">
                             <a data-tab-type="trigger" href="#resetPassword">Forgot Password?</a>
@@ -33,14 +33,14 @@
                     <h1>Sign Up for Free</h1>
                     <form action="/" method="post">
                         <div class="top-row">
-                            <div class="field-wrap"> <label> First Name<span class="req">*</span> </label> <input
+                            <div class="field-wrap"> <label> First Name<span class="req">*</span> </label> <input increment-id="3"
                                        type="text" required autocomplete="off" /> </div>
-                            <div class="field-wrap"> <label> Last Name<span class="req">*</span> </label> <input
+                            <div class="field-wrap"> <label> Last Name<span class="req">*</span> </label> <input increment-id="4"
                                        type="text"required autocomplete="off" /> </div>
                         </div>
-                        <div class="field-wrap"> <label> Email Address<span class="req">*</span> </label> <input
+                        <div class="field-wrap"> <label> Email Address<span class="req">*</span> </label> <input increment-id="5"
                                    type="email"required autocomplete="off" /> </div>
-                        <div class="field-wrap"> <label> Set A Password<span class="req">*</span> </label> <input
+                        <div class="field-wrap"> <label> Set A Password<span class="req">*</span> </label> <input increment-id="6"
                                    type="password"required autocomplete="off" /> </div> <button type="submit"
                                 class="button button-block" />Get Started</button>
                     </form>
@@ -50,14 +50,14 @@
                     <h1>Reset password</h1>
                     <form action="/" method="post">
                         <div class="top-row">
-                            <div class="field-wrap"> <label> First Name<span class="req">*</span> </label> <input
+                            <div class="field-wrap"> <label> First Name<span class="req">*</span> </label> <input increment-id="7"
                                        type="text" required autocomplete="off" /> </div>
-                            <div class="field-wrap"> <label> Last Name<span class="req">*</span> </label> <input
+                            <div class="field-wrap"> <label> Last Name<span class="req">*</span> </label> <input increment-id="8"
                                        type="text"required autocomplete="off" /> </div>
                         </div>
-                        <div class="field-wrap"> <label> Email Address<span class="req">*</span> </label> <input
+                        <div class="field-wrap"> <label> Email Address<span class="req">*</span> </label> <input increment-id="9"
                                    type="email"required autocomplete="off" /> </div>
-                        <div class="field-wrap"> <label> Set A Password<span class="req">*</span> </label> <input
+                        <div class="field-wrap"> <label> Set A Password<span class="req">*</span> </label> <input increment-id="10"
                                    type="password"required autocomplete="off" /> </div> <button type="submit"
                                 class="button button-block" />Get Started</button>
                     </form>
@@ -66,64 +66,118 @@
         </div> <!-- /form -->
     </div>
 
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-
     <script>
-        document.querySelectorAll('[data-tab-type="trigger"]')
-            .forEach(trigger => {
-                trigger.addEventListener('click', function(event) {
-                    event.preventDefault();
+        window.allTabIds = [
+            '#login',
+            '#signup',
+            '#resetPassword',
+        ];
 
-                    document.querySelectorAll('[data-tab-type="header"] li.tab.active')
-                        .forEach(activeMenu => {
-                            activeMenu.classList.remove('active');
+        document.addEventListener('DOMContentLoaded', function() {
+            document.querySelectorAll('[data-tab-type="trigger"]')
+                .forEach(trigger => {
+                    trigger.addEventListener('click', function(event) {
+                        event.preventDefault();
+
+                        document.querySelectorAll('[data-tab-type="header"] li.tab.active')
+                            .forEach(activeMenu => {
+                                activeMenu.classList.remove('active');
+                            });
+
+                        let tabId = (event.target.getAttribute('href') ?? '')?.replaceAll('#', '') ?? '';
+
+                        if (!tabId) {
+                            console.log('No tabId ID')
+                            return null;
+                        }
+
+                        document.querySelectorAll(`ul[data-tab-type="header"] li.tab`)
+                            .forEach(tabTriggerContainer => {
+                                tabTriggerContainer.classList.remove('active');
+                                if (tabTriggerContainer.querySelectorAll(`a[href="#${tabId}"]`).length > 0) {
+                                    tabTriggerContainer.classList.add('active')
+                                }
+                            })
+
+                        document.querySelectorAll('.tab-content > div')
+                            .forEach(async (tabContent) => {
+                                // tabContent.style.display = 'none';
+
+                                tabContent.style.transition = '0.5s';
+                                tabContent.style.opacity = 0;
+
+                                let targetTabContentId = tabContent.getAttribute('id');
+
+                                if (!targetTabContentId) {
+                                    console.log('No targetTabContentId ID')
+                                    return null;
+                                }
+
+                                console.log(targetTabContentId, tabId, (targetTabContentId == tabId));
+
+                                if (targetTabContentId == tabId) {
+                                    fadeIn(tabContent, 400) //.then(() => {});
+                                    return;
+                                }
+
+                                fadeOut(tabContent, 400) //.then(() => {});
+                            });
+
+                    });
+                })
+
+            // Pure JS
+            document.querySelectorAll('.form')
+                .forEach(form => {
+                    form.querySelectorAll('input, textarea')
+                        .forEach(input => {
+                            input.addEventListener('keyup', function(e) {
+                                if (!e.target.value || e.target.value == '') {
+                                    e.target.previousElementSibling.classList.remove('active');
+                                    return;
+                                }
+
+                                e.target.previousElementSibling.classList.add('active', 'highlight');
+                            });
+
+                            input.addEventListener('blur', function(e) { //blur => sai do item
+                                e.target.previousElementSibling.classList.remove('highlight');
+
+                                if (!e.target.value || e.target.value == '') {
+                                    e.target.previousElementSibling.classList.remove('active');
+                                    return;
+                                }
+
+                                e.target.previousElementSibling.classList.add('active');
+                            });
+
+                            input.addEventListener('focus', function(e) {
+
+                                e.target.previousElementSibling.classList.add('highlight');
+                                if (!e.target.value) {
+                                    e.target.previousElementSibling.classList.remove('active');
+                                }
+                            });
+
+                            input.addEventListener('change', function(e) {
+                                if (e.target.value) {
+                                    e.target.previousElementSibling.classList.add('active', 'highlight');
+                                    return;
+                                }
+
+                                e.target.previousElementSibling.classList.remove('highlight');
+                            });
                         });
-
-                    // $(this).parent().addClass('active');
-                    // $(this).parent().siblings().removeClass('active');
-
-                    let tabId = (event.target.getAttribute('href') ?? '')?.replaceAll('#', '') ?? '';
-
-                    if (!tabId) {
-                        console.log('No tabId ID')
-                        return null;
-                    }
-
-                    document.querySelectorAll(`ul[data-tab-type="header"] li.tab`)
-                        .forEach(tabTriggerContainer => {
-                            tabTriggerContainer.classList.remove('active');
-                            if (tabTriggerContainer.querySelectorAll(`a[href="#${tabId}"]`).length > 0) {
-                                tabTriggerContainer.classList.add('active')
-                            }
-                        })
-
-                    document.querySelectorAll('.tab-content > div')
-                        .forEach(async (tabContent) => {
-                            // tabContent.style.display = 'none';
-
-                            tabContent.style.transition = '0.5s';
-                            tabContent.style.opacity = 0;
-
-                            let targetTabContentId = tabContent.getAttribute('id');
-
-                            if (!targetTabContentId) {
-                                console.log('No targetTabContentId ID')
-                                return null;
-                            }
-
-                            // targetTabContentId = `#${targetTabContentId}`;
-
-                            console.log(targetTabContentId, tabId, (targetTabContentId == tabId));
-
-                            if (targetTabContentId == tabId) {
-                                fadeIn(tabContent, 400) //.then(() => {});
-                                return;
-                            }
-
-                            fadeOut(tabContent, 400) //.then(() => {});
-                        });
-
                 });
-            })
+
+
+            if (window.location.hash && window.allTabIds && window.allTabIds.includes(window.location.hash)) {
+                let tabId = window.location.hash;
+                document.querySelectorAll(`[data-tab-type="trigger"][href="${tabId}"]`)
+                    .forEach(tabTrigger => {
+                        tabTrigger.click();
+                    })
+            }
+        });
     </script>
 @endsection
